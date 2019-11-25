93ad1adf2023bd757416bc405f2ac412a6c25b90
Mariana Teut <mary_marianailea@yahoo.com>
2019-11-25 21:31:28 +0200

    added updated product and delete  product

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -6,6 +6,7 @@ import org.fasttrackit.onlineshop.persisteance.ProductRepository;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -40,5 +41,19 @@ public class ProductService {
        return productRepository.findById(id)
                //lambda expression
                .orElseThrow(() -> new ResourceNotFoundException("Product " + id +"does not exist."));
+    }
+    public Product updateProduct(long id ,SaveProductRequest request){
+        LOGGER.info("Updating product {}: {}", id, request);
+
+        Product product = getProduct(id);
+
+        BeanUtils.copyProperties(request, product);
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(long id){
+        LOGGER.info("Deleting product {}", id);
+        productRepository.deleteById(id);
+        LOGGER.info("Deleting product{}", id);
     }
 }
