e9a55d8102b6219b4f42ad77ebca98bedaf9bca6
Mariana Teut <mary_marianailea@yahoo.com>
2020-01-06 19:19:12 +0200

    adding cartController

---
+++ src/main/java/org/fasttrackit/onlineshop/web/CartController.java
@@ -0,0 +1,35 @@
+package org.fasttrackit.onlineshop.web;
+
+import org.fasttrackit.onlineshop.service.CartService;
+import org.fasttrackit.onlineshop.transfer.AddProductToCartRequest;
+import org.fasttrackit.onlineshop.transfer.CartResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@CrossOrigin
+@RestController
+@RequestMapping("*/carts")
+public class CartController {
+
+    private final CartService cartService;
+    @Autowired
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+    @PutMapping
+    public ResponseEntity addProductToCart(
+            @RequestBody @Valid AddProductToCartRequest request){
+        cartService.addProductToCart(request);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CartResponse> getCart(@PathVariable long id){
+        CartResponse cart = cartService.getCart(id);
+        return new ResponseEntity<>(cart, HttpStatus.OK);
+    }
+}
